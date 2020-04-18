fa9c010f0f861094f05cd562b3ce82ac7808dc7e
Anna Guerrero <acg2176@columbia.edu>
2020-04-18 20:25:46 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -13,7 +13,7 @@ class CashRegister
   def add_item(title, price, quantity = 1)
     self.total += price * quantity
       if quantity > 1
-        quantity.times do
+        quantity.times do |title|
           self.items << title
         end
       end
