5ebcfb82f2b4f5d4e36d86dd5b71359e751ea58d
Anna Guerrero <acg2176@columbia.edu>
2020-04-18 19:55:30 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,25 @@
 
+class CashRegister
+  :cash_register
+  :cash_register_with_discount
+  attr_accessor :total, :discount, :items
+
+  def initialize(discount = discount)
+    @total = 0.00
+    @discount = discount
+  end
+
+  def items
+    @items
+  end
+
+  def add_item(title, price, quantity = 1)
+    self.total += price * quantity
+  end
+
+  def apply_discount
+    total_discount = self.discount*0.1 * self.total
+    self.total -= total_discount
+    puts "After the discount, the total comes to $#{self.total}."
+  end
+end
