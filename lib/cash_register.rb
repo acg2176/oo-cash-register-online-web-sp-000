a2231e73c9382ec3838d0d66f14c6da1bee2a4f8
Anna Guerrero <acg2176@columbia.edu>
2020-04-18 20:02:46 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -20,7 +20,7 @@ class CashRegister
   def apply_discount
     if self.discount
       total_discount = self.discount * 0.01 * self.total
-      self.total -= total_discount.ceil
+      self.total -= (total_discount).ceil
       "After the discount, the total comes to $#{self.total}."
     end
   end
